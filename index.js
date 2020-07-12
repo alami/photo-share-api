bffbabcaf0f71b04c97dabdf723ea76684ade101
Aminov <alami@tshtt.uz>
2020-07-12 21:50:24 +0500

    p125 users & photos

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ type Photo {
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
 }
 input PostPhotoInput {
     name: String!
     category: PhotoCategory=PORTRAIT
     description: String
 }
+type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+}
+
 type Query {
     totalPhotos: Int!
     allPhotos: [Photo!]!
@@ -28,7 +36,34 @@ type Mutation {
 }
 `
 var _id = 0
-var photos = []
+// var photos = []
+var users = [
+    { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+    { "githubLogin": "gPlake", "name": "Glen Plake" },
+    { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+var photos = [
+    {
+        "id": "1",
+        "name": "Dropping the Heart Chute",
+        "description": "The heart chute is one of my favorite chutes",
+        "category": "ACTION",
+        "githubUser": "gPlake"
+    },
+    {
+        "id": "2",
+        "name": "Enjoying the sunshine",
+        "category": "SELFIE",
+        "githubUser": "sSchmidt"
+    },
+    {
+        id: "3",
+        "name": "Gunbarrel 25",
+        "description": "25 laps on gunbarrel today",
+        "category": "LANDSCAPE",
+        "githubUser": "sSchmidt"
+    }
+]
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
