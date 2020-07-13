68b169e8b4b3a424a5081292248a2bfe8f5b8391
alami <alami@km.ru>
2020-07-13 19:42:10 +0500

    p137 add 2 fs

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const expressPlayground = require('graphql-playground-middleware-express').default
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
+const { readFileSync } = require('fs')
+// const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+// const resolvers = require('./resolvers')
+
 const typeDefs = `
 enum PhotoCategory {
     SELFIE
@@ -137,6 +142,7 @@ const server = new ApolloServer({
 })
 server.applyMiddleware({ app })
 app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 app.listen({ port: 4000 }, () =>
     console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
 )
